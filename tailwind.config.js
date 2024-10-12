2f8c39bf1d4ad7852f784ce299fff6d2c562c431
GonzaQuedena01 <gquedena1@gmail.com>
2024-10-12 17:35:08 -0500

    chore: initial commit.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './assets/styles/**/*.css',
+    './js/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
