ace38b8e17e782ba0c398867732e5beaa2f6f54e
GonzaQuedena01 <gquedena1@gmail.com>
2024-10-12 21:13:04 -0500

    chore: set design colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './index.html',
@@ -6,8 +7,17 @@ module.exports = {
     './js/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgba(var(--color-primary), 0.8)',
+        secondary: 'var(--color-secondary)'
+      },
+      backgroundImage: theme => ({
+        'linear-grey': 'linear-gradient(to right, #57687C 0%, #B4C7DD 100%)',
+        'linear-red': 'linear-gradient(to left, #DDD8D5 0%, #F05F47 50%)',
+        'linear-green': 'linear-gradient(to right, #51B8AC 0%, #75C842 100%)',
+      }),
+    },
   },
   plugins: [],
 }
-
